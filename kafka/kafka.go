35d6ec60876b9da48e14295dac8031fa435d5222
AhmedSamir <asamir@axisapp.com>
2024-12-03 15:13:46 +0200

    [add]

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -1,6 +1,7 @@
 package kafka
 
 import (
+	"context"
 	"log"
 	"strings"
 
@@ -75,4 +76,80 @@ func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 		c.received <- true
 	}
 	return nil
+}
+
+func CheckProduce(brokers string, topic string) bool {
+	producer, err := CreateProducer(brokers)
+	if err != nil {
+		log.Printf("Error creating Kafka producer: %v", err)
+		return false
+	}
+	defer producer.Close()
+	err = SendMessage(producer, topic, "produce within the health check")
+	if err != nil {
+		log.Printf("Error sending message to Kafka topic %s: %v", topic, err)
+		return false
+	}
+	return true
+}
+
+func CheckConsume(brokers string, topic string, groupID string) bool {
+	consumerGroup, err := CreateConsumer(brokers, groupID)
+	if err != nil {
+		log.Printf("Error creating Kafka consumer group: %v", err)
+		return false
+	}
+	defer consumerGroup.Close()
+
+	consumer := &Consumer{
+		ready:    make(chan bool),
+		received: make(chan bool),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		for {
+			err := consumerGroup.Consume(ctx, []string{topic}, consumer)
+			if err != nil {
+				log.Printf("Error during message consumption: %v", err)
+				return
+			}
+		}
+	}()
+
+	log.Println("Kafka consumer is ready for health check")
+
+	select {
+	case <-consumer.received:
+		log.Println("Consumer received at least one message")
+		return true
+	case <-ctx.Done():
+		log.Println("Context done without consuming messages")
+		return false
+	}
+}
+
+func CheckKafka(brokers string) bool {
+	brokerList := strings.Split(brokers, ",")
+	log.Printf("brokers : %v", brokerList)
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_0_0_0
+	config.ClientID = "health-check-client"
+
+	admin, err := sarama.NewClusterAdmin(brokerList, config)
+	if err != nil {
+		log.Printf("failed to create Kafka admin client: %v", err)
+		return false
+	}
+	defer admin.Close()
+
+	brokersList, _, err := admin.DescribeCluster()
+	if err != nil {
+		log.Printf("failed to describe cluster: %v", err)
+		return false
+	}
+	log.Printf("Kafka health check successful: found %d broker(s)\n", len(brokersList))
+	return true
 }
